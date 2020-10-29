e799b6ef132794b861e0a8d640f4074dcbde096f
hassanaster <hassanaster2@gmail.com>
2020-10-29 15:27:07 -0500

    Deleting directory  wget

---
+++ enunciado/wcat/Sample2.c
@@ -0,0 +1,1 @@
+This is a example for the comand wcat - example 2
--- enunciado/wcat/wcat.c
+++ enunciado/wcat/wcat.c
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]){
     for(int i=1; i< argc; i++){
         if(get_files(argv[i])!=0){
             //when this happends, the file wasn't able to be open
-            printf("This command isn't able to open the file\n");
-            exit(-1); 
+            printf("wcat: cannot open file\n");
+            exit(1); 
         }
     }
     //The command finish to read sucessfully all files
@@ -33,7 +33,7 @@ int get_files(char *file){
         }
         fclose(app);
         printf("\n");
-        //file was reade correctly
+        //file was read correctly
         return 0;
     }
 }
